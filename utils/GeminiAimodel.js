b275c771f353dd1e9a8a4a32efa1c0aad26f6ca1
chamanchangappa03 <137156108+chamanchangappa03@users.noreply.github.com>
2025-10-07 12:00:14 +0530

    flash-latest

--- utils/GeminiAimodel.js
+++ utils/GeminiAimodel.js
@@ -17,7 +17,7 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-1.5-flash-latest",
   });
   
   const generationConfig = {
